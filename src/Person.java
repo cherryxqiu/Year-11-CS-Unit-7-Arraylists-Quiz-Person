857d517538fb30126044f38e160182b99cfc0331
cherryqiu <xiaohuiq2025@student.cis.edu.hk>
2023-03-07 12:50:56 +0800

    completed yay! :)

--- src/Person.java
+++ src/Person.java
@@ -4,4 +4,21 @@ public class Person {
     private String lastName;
     private int age;
 
+    public Person(String f, String l, int a){
+        firstName = f;
+        lastName = l;
+        age = a;
+    }
+
+    public String getFirstName(){return firstName;}
+    public void setFirstName(String firstName){this.firstName=firstName;}
+    public String getLastName(){return lastName;}
+    public void setLastName(String lastName){this.lastName = lastName;}
+    public int getAge(){return age;}
+    public void setAge(int age){this.age = age;}
+
+    public String toString(){
+        return firstName + lastName + age;
+    }
+
 }
--- src/PersonFactory.java
+++ src/PersonFactory.java
@@ -9,14 +9,29 @@ public class PersonFactory {
     }
 
     public void addPerson(Person person) {
+        ArrayList<String> persons = new ArrayList();
 
+        for(int i = 0; i < allPersons.size(); i++){
+            persons.add(allPersons.get(i).getLastName());
+        }
     }
 
     public ArrayList<Person> under18() {
-
+        ArrayList<Person> under18 = new ArrayList();
+        for(int i = 0; i < allPersons.size(); i++){
+            if(allPersons.get(i).getAge() <=18){
+                under18.add(allPersons.get(i));
+            }
+        }
+        return under18;
     }
 
     public boolean checkLastName(String lastName) {
-
+        for(int i = 0; i < allPersons.size(); i++){
+            if(allPersons.get(i).getLastName().equals(lastName)){
+                return true;
+            }
+        }
+        else return false;
     }
 }
